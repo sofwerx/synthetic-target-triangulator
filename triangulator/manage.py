a60bc900ffca3b67f39df5e3b5c18c69a0271b56
Ian Blenke <ian@blenke.com>
2017-10-16 09:46:55 -0400

    Adding submodule for synthetic-target-triangulator-library

--- triangulator/manage.py
+++ triangulator/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "triangulator.settings")
-    os.environ.setdefault("PYTHONPATH", "./lib")
+    os.environ.setdefault("PYTHONPATH", "./synthetic-target-triangulator-library")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
